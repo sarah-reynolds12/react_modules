5d1960cee7dad7bf35d02114a40a035f08912ad0
sarah-reynolds12 <73564744+sarah-reynolds12@users.noreply.github.com>
2021-02-04 22:03:50 -0500

    NYT App

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -12,6 +12,8 @@ import State from '../concepts/State';
 import Effects from "../concepts/Effects";
 import PropsDemo from "../concepts/PropsDemo";
 import Hooks from "../concepts/Hooks";
+import TimePiecesApp from "../apps/timer-apps/TimePiecesApp"
+import NytApp from "../apps/nyt-app/NytApp"
 
 const Sidebar = () => {
     return (
@@ -26,6 +28,8 @@ const Sidebar = () => {
                     <li><Link to = "/propsdemo">PropsDemo</Link></li>
                     <li><Link to = "/hooks">Hooks</Link></li>
                     <li><Link to = "/resources">Resources</Link></li>
+                    <li><Link to = "/timer">Timers</Link></li>
+                    <li><Link to = "/nyt">NYT</Link></li>
 
                 </ul>
             </div>
@@ -40,6 +44,8 @@ const Sidebar = () => {
                     <Route exact path = '/propsdemo'><PropsDemo /></Route>
                     <Route exact path = "/effects"><Effects /></Route>
                     <Route exact path = "/hooks"><Hooks /></Route>
+                    <Route exact path = "/timer"><TimePiecesApp /></Route>
+                    <Route exact path = "/nyt"><NytApp /></Route>
                 </Switch>
             </div>
         </div>
